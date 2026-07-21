09ff95bfa19da2f7ffa0ae7ac59ea1a52c8852a2
snabby474747 <113586130+snabby474747@users.noreply.github.com>
2026-07-21 12:28:45 +0530

    Add: is-anagram - submission-35

---
+++ Data Structures & Algorithms/is-anagram/submission-35.py
@@ -0,0 +1,34 @@
+class Solution:
+    def isAnagram(self, s: str, t: str) -> bool:
+        
+        # if len(s) != len(t):
+        #     return False
+
+        # setS,setT = {},{}
+
+        # for i in range (len(s)):
+        #     setS[s[i]]=1+setS.get(s[i],0)
+        #     setT[t[i]]=1+setT.get(t[i],0)
+
+        # for c in setS:
+        #     if setS[c] != setT.get(c,0):
+        #         return False
+        
+        # return True
+
+        # return Counter(s) == Counter(t) #shortcut solutions
+
+        if len(s)!=len(t):
+            return False
+
+        setS,setT = {},{}
+
+        for i in range (len(s)):
+            setS[s[i]]=1+setS.get(s[i],0)
+            setT[t[i]]=1+setT.get(t[i],0)
+
+        for c in setS:
+            if setS[c] != setT.get(c,0):
+                return False
+        
+        return True
